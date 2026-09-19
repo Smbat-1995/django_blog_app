5ebe7a39f0303a322b0d1c7a61ac47c774e87ce6
Smbat-1995 <harutyunyan-smbat@mail.ru>
2026-09-19 14:45:11 +0400

    Home page

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+posts = [
+    {'title':'Post 1',
+     'content':'Smbat post',
+     'author':'Smbat',
+     'date_posted':'7 January 1995'},
+    {'title':'Post 2',
+     'content':'Arman post',
+        'author':'Arman',
+        'date_posted':'7 July 1999'}
+
+]
+
+def home(request):
+    context = {'posts':posts}
+    return render(request, 'blog/home.html',context=context)
